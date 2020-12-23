6b8cb3ff133172d2f9eefc8164eaef785dc426ae
Avia-Code <57874233+Avia-Code@users.noreply.github.com>
2020-12-24 01:34:56 +0200

    Add changeVolume button to videoPlayer.js
    Add changeVolume button

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,17 +1,23 @@
 export const videoPlayerInit = () => {
+    const playerBlock = document.querySelector('.player-block');
     const videoPlayer = document.querySelector('.video-player');
     const videoButton__play = document.querySelector('.video-button__play');
     const videoButton__stop = document.querySelector('.video-button__stop');
     const videoTime__passed = document.querySelector('.video-time__passed');
     const videoProgress = document.querySelector('.video-progress');
     const videoTime__total = document.querySelector('.video-time__total');
+    const videoVolume = document.querySelector('.video-volume');
+    const videoVolumePlus = document.querySelector('.video-icon--plus');
+    const videoVolumeMinus = document.querySelector('.video-icon--minus');
+    const videoFullscreen = document.querySelector('.video-fullscreen');
 
     const toggleIcon = () => {
         videoButton__play.classList.toggle('fa-play');
         videoButton__play.classList.toggle('fa-pause');
     };
 
-    const togglePlay = () => {
+    const togglePlay = (event) => {
+        event.preventDefault();
         if (videoPlayer.paused) {
             videoPlayer.play();
         }
@@ -25,7 +31,22 @@ export const videoPlayerInit = () => {
         videoPlayer.currentTime = 0;
     };
 
+    const changeVolume = () => {
+        const volume = videoVolume.value;
+        videoPlayer.volume = volume / 100;
+        localStorage.setItem('volumeLevel', volume / 100);
+    };
+
     const addZero = n => n < 10 ? '0' + n : n;
+
+    const changeVolumeProgress = () => {
+        const volume = videoPlayer.volume;
+        videoVolume.value = volume * 100;
+    };
+
+    const toggleFullScreen = () => {
+        videoPlayer.requestFullscreen();
+    }
 
     videoPlayer.addEventListener('click', togglePlay);
 
@@ -59,5 +80,46 @@ export const videoPlayerInit = () => {
 
         videoPlayer.currentTime = value * duration / 100;
     })
+
+    videoVolume.addEventListener('input', changeVolume);
+    // console.log(videoPlayer.volume);
+    changeVolume();
+    // console.log(videoPlayer.volume);
+
+    videoPlayer.addEventListener('volumechange', changeVolumeProgress);
+
+    videoVolumePlus.addEventListener('click', () => {
+
+        if (videoPlayer.volume !== 1) {
+            videoPlayer.volume = 1;
+        }
+        else {
+            videoPlayer.volume = localStorage.getItem('volumeLevel');
+        }
+
+    })
+
+    videoVolumeMinus.addEventListener('click', () => {
+
+        if (videoPlayer.volume !== 0) {
+            videoPlayer.volume = 0;
+        }
+        else {
+            videoPlayer.volume = localStorage.getItem('volumeLevel');
+        }
+
+    })
+
+    videoFullscreen.addEventListener('click', toggleFullScreen);
+
+    document.body.addEventListener('keydown', (event) => {
+        if (event.code === 'KeyF' && playerBlock.classList.contains('active')) {
+            toggleFullScreen();
+            console.dir(toggleFullScreen);
+        }
+    })
+
+    
+
 };
 
